f466ef15cf85471400c243caeb57fb793112d423
Dmytro Lysenko <kratoc.ua@gmail.com>
2026-07-14 22:38:47 +0300

    update project structure and configuration: add routing, update styles, and modify app components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {HomePage} from './pages/home-page/home-page';
+import {AboutPage} from './pages/about-page/about-page';
+import {ContactsPage} from './pages/contacs-page/contacts-page.component';
+import {ProjectsPage} from './pages/projects-page/projects-page';
+import {ProjectDetailsPage} from './pages/project-details-page/project-details-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePage },
+  { path: 'about', component: AboutPage },
+  { path: 'projects', component: ProjectsPage },
+  { path: 'projects/:id', component: ProjectDetailsPage },
+  { path: 'contacts', component: ContactsPage },
+  { path: '**', redirectTo: '' }
+];
