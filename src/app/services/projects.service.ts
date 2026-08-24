cf3a2e1de29ae1a8e2341f1645274261afe6403e
Dmytro Lysenko <kratoc.ua@gmail.com>
2026-08-24 15:40:23 +0300

    update project listings: add new projects with folder names and photo counts

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -16,11 +16,11 @@ export class ProjectsService {
     { id: 4, folder: 'residence', name: 'Residence in Odesa', photoCount: 14 },
     { id: 5, folder: 'glassly-optic', name: 'Glassly Optic', photoCount: 6 },
     { id: 6, folder: 'gym-tonic', name: 'Gym Tonic', photoCount: 7 },
-    { id: 7, folder: '', name: '', photoCount: 0 },
-    { id: 8, folder: '', name: '', photoCount: 0 },
-    { id: 9, folder: '', name: '', photoCount: 0 },
-    { id: 10, folder: '', name: '', photoCount: 0 },
-    { id: 11, folder: '', name: '', photoCount: 0 },
+    { id: 7, folder: 'kadorr-121', name: 'Kadorr 121', photoCount: 10 },
+    { id: 8, folder: 'golden-era', name: 'Golden Era', photoCount: 9 },
+    { id: 9, folder: 'donskogo-house', name: 'Donskogo House', photoCount: 7 },
+    { id: 10, folder: 'chesarskie-poland', name: 'Chesarskie Poland', photoCount: 10 },
+    { id: 11, folder: 'avdeeva-house', name: 'Avdeeva House', photoCount: 9 },
   ];
 
   getProjects(): Project[] {
